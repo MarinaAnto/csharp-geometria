3db82e4a8202aba8b1dd5dea29cb8952f5da31a4
MarinaAnto <antonellaarmas160@gmail.com>
2022-04-08 11:37:40 +0200

    Aggiungere i file di progetto.

---
+++ Geometria/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    internal class Rettangolo
+    {
+        //Attributi
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+        //Costruttore
+        public Rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+        //Metodi
+        public int CalcolaArea()
+        {
+            int risultato = baseRettangolo * altezzaRettangolo;
+            return risultato;
+        }
+        public int CalcolaPerimetro()
+        {
+            int ris = 2 * (baseRettangolo + altezzaRettangolo);
+            return ris;
+        }
+        public void Stampa(int baseRettangolo,int altezzaRettangolo)
+        {
+           Console.WriteLine("Base: " + baseRettangolo+" cm" + "\nAltezza: " + altezzaRettangolo+" cm"+ "\nArea: " + CalcolaArea()+" cm^2" + "\nPerimetro: " +CalcolaPerimetro()+" cm");
+        }
+    }
+}
+
+
+
---
+++ Geometria/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Geometria;
+
+Console.WriteLine("Inserisci la base: ");
+int lato = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci l'altezza: ");
+int altezza = int.Parse(Console.ReadLine());
+Rettangolo rettangolo = new Rettangolo(lato, altezza);
+rettangolo.Stampa(lato,altezza);
+int b = 10;
+int h = 6;
+Rettangolo SecondoRettangolo = new Rettangolo(b, h);
+SecondoRettangolo.Stampa(b, h);
